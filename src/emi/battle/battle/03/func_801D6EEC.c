1bd791bf6afbf3917645dbe0096d4c5ce0646c71
loopyd <nightwintertooth@gmail.com>
2026-07-28 14:59:30 -0700

    feat(decomp): byte-match emi/battle/battle/03@801D6EEC

---
+++ src/emi/battle/battle/03/func_801D6EEC.c
@@ -0,0 +1,5 @@
+#include "internal.h"
+
+void func_801D6EEC(void) {
+  D_801EAD0C[BATTLE_GLOBAL_BYTE_62E2]();
+}
--- src/emi/battle/battle/03/internal.h
+++ src/emi/battle/battle/03/internal.h
@@ -80,6 +80,7 @@ extern Battle03Handler D_801EACD4[];
 extern Battle03Handler D_801EACE8[];
 extern Battle03Handler D_801EACF4[];
 extern Battle03Handler D_801EAD00[];
+extern Battle03Handler D_801EAD0C[];
 extern volatile u8  BATTLE_LOCAL_BYTE_62EC;
 extern volatile u8  BATTLE_GLOBAL_BYTE_62E0;
 extern volatile u8  BATTLE_GLOBAL_BYTE_62E1;
