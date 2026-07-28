962807395ce0c73658795d5a5320a08dda3c02b7
loopyd <nightwintertooth@gmail.com>
2026-07-27 22:25:46 -0700

    feat(decomp): byte-match emi/world00/area026/13@801F3338

---
+++ src/emi/world00/area026/13/func_801F3338.c
@@ -0,0 +1,8 @@
+#include "internal.h"
+
+/* @behavior sets bit 0x2E in the active scenario flags.
+ * @source 0x801F3338
+ */
+void func_801F3338(void) {
+  func_8015B580((void*)D_8014686C, 0x2Eu);
+}
