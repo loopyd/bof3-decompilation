2344beac8ecddaaba070b6ffae84a20cd42eaca2
loopyd <nightwintertooth@gmail.com>
2026-07-29 10:05:20 -0700

    feat(decomp): byte-match emi/battle/battle/03@801E78F0

---
+++ src/emi/battle/battle/03/func_801E78F0.c
@@ -0,0 +1,16 @@
+#include "internal.h"
+
+/* @source 0x801E78F0
+ * @behavior Decrements the active work countdown or flags it and advances scratchpad work.
+ */
+void func_801E78F0(void) {
+    Battle03LocalWork* work;
+
+    work = D_801EB4E0;
+    if (work->unk_40 != 0) {
+        work->unk_40 -= 0x2000;
+    } else {
+        work->flags_00 |= 0x40;
+        SPAD_PTR_SLOT(u8, 0x44)[1]++;
+    }
+}
