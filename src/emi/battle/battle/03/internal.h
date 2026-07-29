351c0ecfa8070e2f003d849cd32dec37331ef6fe
loopyd <nightwintertooth@gmail.com>
2026-07-28 18:06:24 -0700

    feat(decomp): byte-match emi/battle/battle/03@801E3A00

--- src/emi/battle/battle/03/internal.h
+++ src/emi/battle/battle/03/internal.h
@@ -89,6 +89,7 @@ extern Battle03Handler D_801EB1F4[];
 extern Battle03Handler D_801EB3E4[];
 extern Battle03Handler D_801EB3EC[];
 extern Battle03Handler D_801EB3F4[];
+extern Battle03Handler D_801EB404[];
 extern volatile u8  BATTLE_LOCAL_BYTE_62EC;
 extern volatile u8  BATTLE_GLOBAL_BYTE_62E0;
 extern volatile u8  BATTLE_GLOBAL_BYTE_62E1;
