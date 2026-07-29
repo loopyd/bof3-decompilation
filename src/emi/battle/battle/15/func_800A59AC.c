8b4365d06a12c97daa9317396cfe1978c2268bfe
loopyd <nightwintertooth@gmail.com>
2026-07-28 17:00:33 -0700

    feat(decomp): byte-match emi/battle/battle/15@800A59AC

---
+++ src/emi/battle/battle/15/func_800A59AC.c
@@ -0,0 +1,8 @@
+#include "internal.h"
+
+/* @source 0x800A59AC
+ * @behavior dispatches the current battle-selection handler from D_800B4D14.
+ */
+void func_800A59AC(void) {
+  D_800B4D14[D_801462E4]();
+}
