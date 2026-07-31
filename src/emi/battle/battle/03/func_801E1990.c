139b863ff63282dfeb7c7956af8ae5b747a476b8
loopyd <nightwintertooth@gmail.com>
2026-07-31 16:56:48 -0700

    feat(decomp): byte-match battle 03 handler dispatch

---
+++ src/emi/battle/battle/03/func_801E1990.c
@@ -0,0 +1,6 @@
+#include "internal.h"
+
+void func_801E1990(void) {
+    D_801EB258[SPAD_PTR_SLOT(u8, 0x44)[2]]();
+    func_801E1B2C();
+}
--- src/emi/battle/battle/03/internal.h
+++ src/emi/battle/battle/03/internal.h
@@ -113,6 +113,7 @@ extern volatile AbilityObject       ABILITY_OBJECTS[];
 extern u8                           D_80144955;
 extern u8                           D_801462F3;
 extern u8                           D_801462F4;
+extern Battle03Handler              D_801EB258[];
 extern u8                           D_801EB2E8[];
 extern Battle03LocalWork            D_80145E90[];
 extern volatile u16           D_80143C40;
