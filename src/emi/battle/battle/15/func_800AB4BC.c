e82d37cce7fdb39b45a46534351392ffd6f41913
loopyd <nightwintertooth@gmail.com>
2026-08-01 15:13:55 -0700

    feat(decomp): byte-match emi/battle/battle/15@0x800AB4BC

--- src/emi/battle/battle/15/func_800AB4BC.c
+++ src/emi/battle/battle/15/func_800AB4BC.c
@@ -8,8 +8,8 @@
 s32 func_800AB4BC(s32 arg0) {
   u16 val;
 
-  val = (*(volatile u16*)((u32)D_80145FAA + ((arg0 & 0xFF) * 0x140)));
-  if (val == 0xE)
-    return 1;
-  return val == 0x128;
+  val = D_80145FAA[(arg0 & 0xFF) * 0xA0];
+  if (val != 0xE)
+    return val == 0x128;
+  return 1;
 }
