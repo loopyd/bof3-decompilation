fba975a1e286184a9e490ff7856a33f5e167fc22
loopyd <nightwintertooth@gmail.com>
2026-08-01 15:04:21 -0700

    feat(decomp): byte-match emi/etc/shop/00@0x801E4460

--- src/emi/etc/shop/00/func_801E4460.c
+++ src/emi/etc/shop/00/func_801E4460.c
@@ -7,13 +7,12 @@
  */
 void func_801E4460(void) {
   PanelTask* task_root;
-  u16        next_val;
 
   task_root = D_80148648;
-  next_val = (u16)((*(volatile u16*)((u8*)task_root + 6)) + 0x10);
-  *(volatile u16*)((u8*)task_root + 6) = next_val;
-  if ((s16)next_val >= 0xF1) {
-    *(volatile u16*)((u8*)task_root + 6) = 0xF0;
+  if ((s16)(*(u16*)((u8*)task_root + 6) =
+                  (u16)(*(u16*)((u8*)task_root + 6) + 0x10)) >=
+      0xF1) {
+    *(u16*)((u8*)task_root + 6) = 0xF0;
     task_root->state = 0;
   }
 }
