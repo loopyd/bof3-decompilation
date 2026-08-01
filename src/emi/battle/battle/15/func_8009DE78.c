f4f09cbe873d57351adaea13f8b122c8f1d68d39
loopyd <nightwintertooth@gmail.com>
2026-08-01 11:40:19 -0700

    feat(decomp): byte-match emi/battle/battle/15@0x8009DE78

--- src/emi/battle/battle/15/func_8009DE78.c
+++ src/emi/battle/battle/15/func_8009DE78.c
@@ -6,8 +6,8 @@
  * @source 0x8009DE78
  */
 void func_8009DE78(void) {
-  volatile u16* ptr;
+  s16* ptr;
 
-  ptr = (volatile u16*)D_801463A0;
-  *(ptr + 2) = -100u;
+  ptr = (s16*)D_801463A0;
+  *(ptr + 2) = -100;
 }
