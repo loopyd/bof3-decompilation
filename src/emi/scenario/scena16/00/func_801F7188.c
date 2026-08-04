b5f6fdb78cc2ffcb820c1c6f28ff92f42a678924
loopyd <nightwintertooth@gmail.com>
2026-08-04 11:36:59 -0700

    feat(decomp): byte-match emi/scenario/scena16/00@0x801F7188

--- src/emi/scenario/scena16/00/func_801F7188.c
+++ src/emi/scenario/scena16/00/func_801F7188.c
@@ -4,7 +4,8 @@
  * @source 0x801F7188
  */
 void func_801F7188(void) {
-  u32 selection;
+  u8 selection;
+  u8* slot;
 
   if (SCENA16_D_80143C40 == 0u) {
     SCENA16_D_80143C30 = 0u;
@@ -12,16 +13,17 @@ void func_801F7188(void) {
     func_801F845C();
     game_queue_frontend_cue(0x213u);
     game_queue_frontend_cue(0x214u);
-    selection = (u32)SCENA16_D_80145029;
+    slot = (u8*)&SCENA16_D_80145029;
+    selection = *slot;
 
     if (selection != 0xffu) {
       u32 selection_offset;
 
       selection_offset = selection << 2;
       game_stop_selection_fx(
-          (u32)SCENA16_SELECTION_FX_TABLE[selection_offset + 0u],
-          (s32)SCENA16_SELECTION_FX_TABLE[selection_offset + 1u]);
-      SCENA16_D_80145029 = 0xffu;
+          (u32)SCENA16_D_80181EBA[selection_offset + 0u],
+          (s32)SCENA16_D_80181EBA[selection_offset + 1u]);
+      *slot = 0xffu;
     }
 
     func_8016C0C0(0x7f, 0x7f);
