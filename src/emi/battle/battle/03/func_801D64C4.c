cbc944678d9eeb3f89cedff99f9fe51189911903
loopyd <nightwintertooth@gmail.com>
2026-08-07 00:59:35 -0700

    feat(decomp): byte-match emi/battle/battle/03@0x801D64C4

--- src/emi/battle/battle/03/func_801D64C4.c
+++ src/emi/battle/battle/03/func_801D64C4.c
@@ -6,18 +6,20 @@
  * @source 0x801D64C4
  */
 u8 func_801D64C4(u32 arg0) {
+  u8  idx;
   u32 flags;
 
-  if (arg0 < 3u) {
+  idx = (u8)arg0;
+  if (idx < 3u) {
     if (BATTLE_GLOBAL_BYTE_63CE != 0u) {
-      flags = BATTLE_LOCAL_WORD_128(&BATTLE_LOCAL_WORK_ARRAY[arg0]);
+      flags = D_80145E90[idx].unk_128;
       if ((flags & 0x10u) == 0u) {
         return 1u;
       }
     }
   } else {
     if (BATTLE_GLOBAL_BYTE_63CE != 0u) {
-      flags = BATTLE_ENEMY_WORD_104(&BATTLE_ENEMY_WORK_ARRAY[arg0 - 3u]);
+      flags = D_801EB630[idx - 3u].unk_104;
       if ((flags & 0x10u) == 0u) {
         return 1u;
       }
