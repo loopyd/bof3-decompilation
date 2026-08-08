e594550c67813c606234f101fb541d444923a711
loopyd <nightwintertooth@gmail.com>
2026-08-08 05:31:54 -0700

    feat(decomp): byte-match func_801F35E0

---
+++ src/emi/world00/area027/13/func_801F35E0.c
@@ -0,0 +1,13 @@
+#include "internal.h"
+
+/**
+ * @source 0x801F35E0
+ * @behavior Resets a state value and conditionally enables a pair of flags.
+ */
+void func_801F35E0(void) {
+  WORLD00_AREA027_STATE_90A8 = 0xFFFF;
+  if (!(WORLD00_AREA027_FLAGS_90C7 & 1)) {
+    WORLD00_AREA027_FLAG_48EB = 1;
+    WORLD00_AREA027_FLAG_48EC = 0;
+  }
+}
