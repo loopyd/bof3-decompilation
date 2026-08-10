3be44aef577d699ad0e2125e9ddb65fc8db377c8
loopyd <nightwintertooth@gmail.com>
2026-08-10 04:03:56 -0700

    feat(decomp): byte-match func_8009DA94

---
+++ src/bof3/battle/func_8009DA94.c
@@ -0,0 +1,10 @@
+#include "bof3/battle/battle15_internal.h"
+
+/**
+ * @source 0x8009DA94
+ * @behavior Invoke the local battle update helper.
+ */
+void func_8009DA94(void)
+{
+  func_800A3F28();
+}
